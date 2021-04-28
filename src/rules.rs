bf75dcf321e16115a0ea440b58478730c3b871e7
Edgar Onghena <dev@edgar.bzh>
2021-04-28 12:57:51 +0200

    Start working on dnf_to_cnf Doesn't work or compile

--- src/rules.rs
+++ src/rules.rs
@@ -1,6 +1,23 @@
 use crate::cnf::{CNFFile, Literal};
 use crate::Grid;
 
+fn dnf_to_cnf(dnf: &[&[Literal]]) -> Vec<Vec<Literal>> {
+    match dnf {
+        [] => unimplemented!(),
+        [con_clause] => con_clause.map(|lit| vec![lit]).collect(),
+        [start @ .., cnf1] => {
+            let cnf2 = dnf_to_cnf(start);
+
+            cnf2.into_iter()
+                .map(|clause| {
+                    // disjonction de cnf1 (FNC) et de clause (clause) -> FNC
+                })
+                .flatten()
+                .collect()
+        }
+    }
+}
+
 pub fn write_rule_1<W>(out: &mut CNFFile<W>, grid: &Grid) {
     // TODO
 }
